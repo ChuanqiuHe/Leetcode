c1f1fb2d9d62e87053729327089ac939b4ef12cd
q_H <83722156+ChuanqiuHe@users.noreply.github.com>
2022-08-14 22:57:51 -0500

    Update min_health(一滴血)_12.java

--- Amazon OA/min_health(一滴血)_12.java
+++ Amazon OA/min_health(一滴血)_12.java
@@ -11,7 +11,9 @@ It's better to fill up armor, In order to achieve this goal , Of course, you sho
 If the level that loses the most blood loses more blood than armor, Then you can fill up armor; 
 If the level that loses the most blood loses less than armor, Then lose as much blood and make up as much .
 */
-、、、为了把补血包尽可能多用，就要在遇到最大伤害值的时候用掉（两者取min）、、、// list<>解法 https://leetcode.com/playground/DFo4yDzt
+、、、为了把补血包尽可能多用，就要在遇到最大伤害值的时候用掉（两者取min）、、、
+// list<>解法 : https://leetcode.com/playground/DFo4yDzt 
+// https://curly-oriole-764.notion.site/22ec8523c820410490092ace51489509?v=a65d5dfe1dae443385cd3bbd0aa3ce9a&p=630bfaa7f2b84e5dbacd002253dc1b90&pm=s
 
 // "static void main" must be defined in a public class.
 public class Main {
